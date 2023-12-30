4a7263f1b068f8803e073e2e68efb84b10df35a6
4dsherwood <4dsherwood@users.noreply.github.com>
2023-12-29 17:05:39 -0700

    - R mgroup like functions together, put no params first

--- Java/src/main/java/parrot/Parrot.java
+++ Java/src/main/java/parrot/Parrot.java
@@ -34,15 +34,15 @@ public class Parrot {
         };
     }
 
+    private double getBaseSpeed() {
+        return 12.0;
+    }
+
     private double getBaseSpeed(double voltage) {
         return Math.min(24.0, voltage * getBaseSpeed());
     }
 
     private double getLoadFactor() {
         return 9.0;
-    }
-
-    private double getBaseSpeed() {
-        return 12.0;
     }
 }
